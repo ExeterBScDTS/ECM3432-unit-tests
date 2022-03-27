7f848be9a66b43cf1b8f452d132447b5ba7eaed0
Michael Saunby <mike@saunby.net>
2022-03-27 20:42:48 +0100

    .

--- code/isemail.py
+++ code/isemail.py
@@ -3,10 +3,17 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-def is_email_addr(text: str) -> bool:    
-    match = re.match("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$", text, re.IGNORECASE)
+
+def is_email_addr(text: str) -> bool:
+    """
+    Use a regular expression to check if 'text' is a correctly formed
+    email address.
+    """
+    regex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$"
+    match = re.match(regex, text, re.IGNORECASE)
     logging.debug(f"regex match is {match}")
     return match != None
+
 
 if __name__ == "__main__":
     assert is_email_addr("m@saun.by"), "d m@saun.by is a valid email address"
